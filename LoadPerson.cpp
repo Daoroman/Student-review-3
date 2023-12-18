b3b01527f6d7f6f01fe211e7e74846b127c4f608
Daoroman <132341235+Daoroman@users.noreply.github.com>
2023-12-18 14:31:41 +0700

    Update LoadPerson.cpp

--- LoadPerson.cpp
+++ LoadPerson.cpp
@@ -1,12 +1,11 @@
-
-bool IsConnected(string_view db_name, int db_connection_timeout,  DBHandler db, DBConnector connector) {
+bool IsConnected(string_view db_name, int db_connection_timeout, DBHandler db, DBConnector connector) {
     if (db_name.starts_with("tmp."s)) {
         db = connector.ConnectTmp(db_name, db_connection_timeout);
     }
     else {
         db = connector.Connect(db_name, db_connection_timeout);
     }
-       return ! (!db_allow_exceptions && !db.IsOK());
+    return !(!db_allow_exceptions && !db.IsOK());
 }
 
 DBQuery BuildQuery(int min_age, int max_age, string_view name_filter, DBHandler db) {
@@ -15,7 +14,7 @@ DBQuery BuildQuery(int min_age, int max_age, string_view name_filter, DBHandler 
         << "select Name, Age "s
         << "where Age between "s << min_age << " and "s << max_age << " "s
         << "and Name like '%"s << db.Quote(name_filter) << "%'"s;
-    DBQuery query(query_str.str());
+   return DBQuery query(query_str.str());
 
 }
 
@@ -26,9 +25,9 @@ vector<Person> LoadPersons(string_view db_name, int db_connection_timeout, bool 
     DBConnector connector(db_allow_exceptions, db_log_level);
     DBHandler db;
 
-    if (!IsConnected(db_name, db_connection_timeout, db_allow_exceptions, db_log_level, db, connector)) {
+     if (!IsConnected(db_name, db_connection_timeout, db_log_level, connector, db) && !db_allow_exceptions) {
         return {};
-  }
+    }
     // Добавить комментарии к DBQuery
     DBQuery query = BuildQuery(min_age, max_age, name_filter, db);
 
